bd86fb9b3de4130b835cdc10a99f97e7d86da430
durgaprasadraju <durgaprasadraju@gmail.com>
2026-05-28 17:55:08 +0530

    cloud restrict access

--- day-3-aws-secrets/terraform/main.tf
+++ day-3-aws-secrets/terraform/main.tf
@@ -2,95 +2,42 @@ provider "aws" {
   region = var.region
 }
 
+# Get AWS Account ID
 data "aws_caller_identity" "current" {}
 
-/* # Create ECR Repository
-resource "aws_ecr_repository" "app_repo" {
-  name = "go-web-app"
-} */
-
-# IAM Role for EC2
-resource "aws_iam_role" "ec2_role" {
-  name = "go-web-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# IAM Policy for EC2
-resource "aws_iam_role_policy" "ec2_policy" {
-  name = "go-web-policy"
-  role = aws_iam_role.ec2_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-
-    Statement = [
-      {
-        Effect = "Allow"
-
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage"
-        ]
-
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-
-        Action = [
-          "secretsmanager:GetSecretValue"
-        ]
-
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# Attach IAM Role to EC2
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "go-web-profile"
-  role = aws_iam_role.ec2_role.name
-}
-
-# Security Group
+# -----------------------------
+# SECURITY GROUP
+# -----------------------------
 resource "aws_security_group" "go_sg" {
-  name = "go-web-sg"
+  name = "go-web-sg-v3"
 
   ingress {
+    description = "HTTP"
+
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
+    description = "Application"
+
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
+
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
+    description = "SSH"
+
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -98,23 +45,33 @@ resource "aws_security_group" "go_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "go-web-sg-v3"
   }
 }
 
-# EC2 Instance
+# -----------------------------
+# EC2 INSTANCE
+# -----------------------------
 resource "aws_instance" "go_server" {
-  ami                    = "ami-0c7217cdde317cfec"
-  instance_type          = "t2.micro"
-  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  vpc_security_group_ids = [aws_security_group.go_sg.id]
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [
+    aws_security_group.go_sg.id
+  ]
 
   user_data = templatefile("${path.module}/user-data.sh", {
     region     = var.region
     account_id = data.aws_caller_identity.current.account_id
+    app_secret = var.app_secret
   })
 
   tags = {
-    Name = "go-web-server"
+    Name = "go-web-server-v3"
   }
 }
--- day-3-aws-secrets/terraform/variables.tf
+++ day-3-aws-secrets/terraform/variables.tf
@@ -1,3 +1,7 @@
 variable "region" {
   default = "us-east-1"
 }
+
+variable "app_secret" {
+  default = "my-super-secret-key"
+}

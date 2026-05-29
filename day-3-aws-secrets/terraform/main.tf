37b232ed034864432d5dcb263b072797c232950d
durgaprasadraju <durgaprasadraju@gmail.com>
2026-05-29 07:46:44 +0530

    assign ec2 role

--- day-3-aws-secrets/terraform/main.tf
+++ day-3-aws-secrets/terraform/main.tf
@@ -2,14 +2,55 @@ provider "aws" {
   region = var.region
 }
 
-# Get AWS Account ID
+# -----------------------------------
+# GET AWS ACCOUNT ID
+# -----------------------------------
 data "aws_caller_identity" "current" {}
 
-# -----------------------------
+# -----------------------------------
+# IAM ROLE FOR EC2
+# -----------------------------------
+resource "aws_iam_role" "ec2_ecr_role" {
+  name = "ec2-ecr-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# -----------------------------------
+# ATTACH ECR READ POLICY
+# -----------------------------------
+resource "aws_iam_role_policy_attachment" "ecr_readonly" {
+  role       = aws_iam_role.ec2_ecr_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# -----------------------------------
+# INSTANCE PROFILE
+# -----------------------------------
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-ecr-profile"
+  role = aws_iam_role.ec2_ecr_role.name
+}
+
+# -----------------------------------
 # SECURITY GROUP
-# -----------------------------
+# -----------------------------------
 resource "aws_security_group" "go_sg" {
-  name = "go-web-sg-v3"
+  name = "go-web-sg-v4"
 
   ingress {
     description = "HTTP"
@@ -22,22 +63,13 @@ resource "aws_security_group" "go_sg" {
   }
 
   ingress {
-    description = "Application"
-
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     description = "SSH"
 
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
 
+    # Replace with your IP
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -50,16 +82,18 @@ resource "aws_security_group" "go_sg" {
   }
 
   tags = {
-    Name = "go-web-sg-v3"
+    Name = "go-web-sg-v4"
   }
 }
 
-# -----------------------------
+# -----------------------------------
 # EC2 INSTANCE
-# -----------------------------
+# -----------------------------------
 resource "aws_instance" "go_server" {
   ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
   vpc_security_group_ids = [
     aws_security_group.go_sg.id
@@ -72,6 +106,6 @@ resource "aws_instance" "go_server" {
   })
 
   tags = {
-    Name = "go-web-server-v3"
+    Name = "go-web-server-v4"
   }
 }
